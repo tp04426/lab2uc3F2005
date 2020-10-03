39d5e08e34558083b672a4d9f3a767ae74671c6c
tp04426 <72290516+tp04426@users.noreply.github.com>
2020-10-03 13:31:51 +0800

    Update HelloWorld.php

--- HelloWorld.php
+++ HelloWorld.php
@@ -3,7 +3,20 @@
     <title>Display Hello World!</title>
   </head>
 <body>
-  <p> Hello World! </p>
+  <p> Kes Covid 19 terkini 2/10/2020 sebanyak 287 kes baharu.
+
+1. Perak - 3
+2.Kedah - 129
+3. Selangor - 31
+4. Kuala Lumpur - 31
+5. Putrajaya - 1 
+6. Negeri sembilan - 2
+7. Johor - 1 
+8. Pahang - 1
+9. Terengganu - 3
+10. Kelantan - 2
+11. Labuan - 1
+12. Sabah - 113</p>
 </body>
 <footer><small>&copy; Copyright 2019, MayDay Corporation</small>
 </footer>
